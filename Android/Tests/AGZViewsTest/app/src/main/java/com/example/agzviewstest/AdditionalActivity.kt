0a3e272e39e88050c937f7364bc18dcb6164b92d
Andrew Guz <andrew.guz.spb@gmail.com>
2023-01-13 12:39:03 +0300

    Exmaple hot to show/change text, read numbers and switch between activities.

---
+++ Android/Tests/AGZViewsTest/app/src/main/java/com/example/agzviewstest/AdditionalActivity.kt
@@ -0,0 +1,11 @@
+package com.example.agzviewstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdditionalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additional)
+    }
+}
