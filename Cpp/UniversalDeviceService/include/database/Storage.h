1319e7b12d9ee48b51c24599407a4620ad8d872c
Andrew Guz <andrew.guz.spb@gmail.com>
2023-01-18 20:49:48 +0300

    Initial databases creation.

--- Cpp/UniversalDeviceService/include/database/Storage.h
+++ Cpp/UniversalDeviceService/include/database/Storage.h
@@ -1,6 +1,7 @@
 #ifndef _STORAGE_H_
 #define _STORAGE_H_
 
+#include <string>
 #include <sqlite3.h>
 
 class Storage
@@ -9,6 +10,11 @@ public:
     Storage();
 
     ~Storage();
+
+private:
+    void InitializeDb();
+
+    void Execute(const std::string& query);
 
 private:
     sqlite3* _connection = nullptr;
--- Cpp/UniversalDeviceService/source/database/Storage.cpp
+++ Cpp/UniversalDeviceService/source/database/Storage.cpp
@@ -1,6 +1,14 @@
 #include "Storage.h"
 
+#include <iostream>
+#include <vector>
+
 #include "PathHelper.h"
+
+int NoActionCallback(void* NotUsed, int argc, char** argv, char** azColName)
+{
+   return 0;
+}
 
 std::string DbPath()
 {
@@ -10,9 +18,32 @@ std::string DbPath()
 Storage::Storage()
 {
     sqlite3_open(DbPath().c_str(), &_connection);
+    InitializeDb();
 }
 
 Storage::~Storage()
 {
     sqlite3_close(_connection);
+}
+
+void Storage::InitializeDb()
+{
+    std::vector<std::string> queries
+    {
+        "CREATE TABLE IF NOT EXISTS 'Devices' ('id' TEXT UNIQUE, 'type' TEXT, 'timestamp' TEXT, PRIMARY KEY('id'))",
+        "CREATE TABLE IF NOT EXISTS 'Thermometers' ('id' TEXT, 'timestamp' TEXT, 'value' REAL)"
+    };
+    for(auto& query : queries)
+        Execute(query);
+}
+
+void Storage::Execute(const std::string& query)
+{
+    char* error = nullptr;
+    int result = sqlite3_exec(_connection, query.c_str(), NoActionCallback, this, &error);
+    if (result != SQLITE_OK)
+    {   
+        std::cout << "SQL error: " << error << std::endl;
+        sqlite3_free(error);
+    }
 }
