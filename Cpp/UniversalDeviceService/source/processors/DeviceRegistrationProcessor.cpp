b2a2ee171ae9a00668b668345e2b3136aa27a0a3
Andrew Guz <andrew.guz.spb@gmail.com>
2023-01-19 21:52:39 +0300

    Known devices will be stored in database.

--- Cpp/UniversalDeviceService/source/processors/DeviceRegistrationProcessor.cpp
+++ Cpp/UniversalDeviceService/source/processors/DeviceRegistrationProcessor.cpp
@@ -1,12 +1,26 @@
 #include "DeviceRegistrationProcessor.h"
 
+#include <sstream>
+
+#include "TimeHelper.h"
+
 DeviceRegistrationProcessor::DeviceRegistrationProcessor(IQueryExecutor* queryExecutor) :
     BaseProcessorWithQueryExecutor(queryExecutor)
 {
 
 }
 
-void DeviceRegistrationProcessor::ProcessMessage(const std::chrono::system_clock::time_point& timestamp, const Message &message)
+void DeviceRegistrationProcessor::ProcessMessage(const std::chrono::system_clock::time_point& timestamp, const Message& message)
 {
-    //TODO
+    std::stringstream queryStream;
+    queryStream
+        << "INSERT OR REPLACE INTO 'Devices' ('id', 'type', 'timestamp') VALUES ('"
+        << message._header._id.data()
+        << "', '" 
+        << message._header._type 
+        << "', '" 
+        << TimeHelper::TimeToString(timestamp)
+        << "')";
+    queryStream.flush();
+    _queryExecutor->Execute(queryStream.str());
 }
--- Cpp/UniversalDeviceSimulators/SimulatorLib/source/RequestAddress.cpp
+++ Cpp/UniversalDeviceSimulators/SimulatorLib/source/RequestAddress.cpp
@@ -6,5 +6,6 @@ std::string RequestAddress::BuildUrl() const
 {
     std::stringstream url;
     url << "http://" << _ip << ":" << _port << _api;
+    url.flush();
     return url.str();
 }
