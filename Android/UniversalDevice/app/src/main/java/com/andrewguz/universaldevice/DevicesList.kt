f4aaa4de1d4f418cbc8411068948bf90c96343ef
Andrew Guz <andrew.guz.spb@gmail.com>
2023-01-22 17:35:21 +0300

    First empty version of UniversalDevice Android application.

---
+++ Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/DevicesList.kt
@@ -0,0 +1,11 @@
+package com.andrewguz.universaldevice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DevicesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devices_list)
+    }
+}
