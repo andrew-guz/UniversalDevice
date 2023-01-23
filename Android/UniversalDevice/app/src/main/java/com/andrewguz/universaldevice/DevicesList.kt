5e78b5bdb99aa49181d367bf9ac511e6f010f10b
Andrew Guz <andrew.guz.spb@gmail.com>
2023-01-23 13:49:44 +0300

    Stupid ans sync (but working) method to do get request.

--- Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/DevicesList.kt
+++ Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/DevicesList.kt
@@ -1,11 +1,43 @@
 package com.andrewguz.universaldevice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.URL
+
 
 class DevicesList : AppCompatActivity() {
+    var client: OkHttpClient = OkHttpClient();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_devices_list)
+
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        println("!!!")
+        var result = getRequest("http://192.168.1.187:8080/api/client/list");
+        println("!!! - {$result}")
+    }
+
+    private fun getRequest(sUrl: String): String? {
+        var result: String? = null
+        try {
+            // Create URL
+            val url = URL(sUrl)
+            // Build request
+            val request = Request.Builder().url(url).build()
+            // Execute request
+            val response = client.newCall(request).execute()
+            result = response.body?.string()
+        }
+        catch(err:Error) {
+            print("Error when executing get request: "+err.localizedMessage)
+        }
+        return result
     }
 }
