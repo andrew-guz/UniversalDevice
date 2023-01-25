d17848fa3098eb3e07cd6fa2a146ca18058573f2
andrew-guz <andrew.guz.spb@gmail.com>
2023-01-25 19:01:43 +0300

    More or less no red now after Gradle sync.

--- Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/DevicesList.kt
+++ Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/DevicesList.kt
@@ -2,9 +2,11 @@ package com.andrewguz.universaldevice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.net.URL
+import kotlinx.serialization.decodeFromString
 import java.io.IOException
 import okhttp3.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
 class DevicesList : AppCompatActivity() {
     var requestWrapper = RequestWrapper()
@@ -13,15 +15,21 @@ class DevicesList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_devices_list)
 
-        requestWrapper.GetRequest("http://188.242.186.162:7315/api/client/list", object : Callback{
+        //requestWrapper.GetRequest("http://188.242.186.162:7315/api/client/list", object : Callback{
+        requestWrapper.GetRequest("http://192.168.1.187:7315/api/client/list", object : Callback{
             override fun onFailure(call: Call, e: IOException) {
+                println("Failed!")
                 e.printStackTrace()
             }
             override fun onResponse(call: Call, response: Response) {
                 response.use {
                     if (!response.isSuccessful)
                         throw IOException("Unexpected code $response")
-                    println(response.body!!.string())
+                    println("!!!")
+                    var bodyData = response.body!!.string();
+                    println(bodyData);
+                    var deviceDescriptions = Json.decodeFromString<List<ExtendedDeviceDescription>>(bodyData);
+                    println(deviceDescriptions.size);
                 }
             }
         });
--- Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/SerializableData.kt
+++ Android/UniversalDevice/app/src/main/java/com/andrewguz/universaldevice/SerializableData.kt
@@ -1,22 +1,22 @@
 package com.andrewguz.universaldevice
 
 import kotlinx.serialization.*
-import kotlinx.serialization.descriptors.*
-import kotlinx.serialization.internal.*
-import kotlinx.serialization.builtins.*
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
-import java.util.UUID
 
 @Serializable
 class ThermometerCurrentValue(
     val value: Float = 0.0f)
 
 @Serializable
-class DeviceDescription(
+open class DeviceDescription(
     val type: String = "",
     val id: String = "")
+
+@Serializable
+class ExtendedDeviceDescription(
+    val type: String = "",
+    val id: String = "",
+    val name: String = "",
+    val timestamp: String = "")
 
 @Serializable
 class MessageHeader(
