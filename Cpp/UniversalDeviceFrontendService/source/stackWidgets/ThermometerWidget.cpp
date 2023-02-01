36e10fcce0e5ea04aa2a404eab990dfd1b1128fe
andrew-guz <andrew.guz.spb@gmail.com>
2023-02-01 11:59:22 +0300

    Auto update thermometer widget every 5 seconds.

--- Cpp/UniversalDeviceFrontendService/source/stackWidgets/ThermometerWidget.cpp
+++ Cpp/UniversalDeviceFrontendService/source/stackWidgets/ThermometerWidget.cpp
@@ -1,6 +1,7 @@
 #include "ThermometerWidget.h"
 
 #include <Wt/WPushButton.h>
+#include <Wt/WTimer.h>
 
 #include "Defines.h"
 #include "Constants.h"
@@ -30,18 +31,25 @@ ThermometerWidget::ThermometerWidget(IStackHolder* stackHolder, const Settings& 
     refresh->setMaximumSize(refresh->maximumWidth(), 50);
     _mainText = _mainLayout->addWidget(std::make_unique<WText>(), 1, 2, AlignmentFlag::Center);
     _mainText->setText(WidgetHelper::TextWithFontSize(0.0f, 80));
+    auto refreshTimer = addChild(std::make_unique<WTimer>());
+    refreshTimer->setInterval(std::chrono::seconds(5));
+    refreshTimer->timeout().connect([&](){ Initialize(_deviceId.data()); });
+    refreshTimer->start();
 }
 
 void ThermometerWidget::Initialize(const std::string& data)
 {   
     _deviceId =  Uuid(data);
+    if (_deviceId.isEmpty())
+        return;
     ComponentDescription messageData;
     messageData._type = Constants::DeviceTypeThermometer;
     messageData._id = _deviceId;
     auto postMessage = MessageHelper::Create(Constants::FrontendServiceType, Uuid::Empty(), Constants::ClientServiceType, Uuid::Empty(), Constants::SubjectGetDeviceInformation, messageData.ToJson());
     auto replyJson = RequestHelper::DoPostRequestWithAnswer({ "127.0.0.1", _settings._servicePort, API_CLIENT_DEVICE }, postMessage.ToJson());
     auto thermometerValues = JsonExtension::CreateVectorFromJson<ExtendedThermometerCurrentValue>(replyJson);
-    if (!thermometerValues.size())
-        return;
-    _mainText->setText(WidgetHelper::TextWithFontSize(thermometerValues.begin()->_value, 80));
+    if (thermometerValues.size())
+        _mainText->setText(WidgetHelper::TextWithFontSize(thermometerValues.begin()->_value, 80));
+    else
+        _mainText->setText(WidgetHelper::TextWithFontSize(0.0f, 80));
 }
