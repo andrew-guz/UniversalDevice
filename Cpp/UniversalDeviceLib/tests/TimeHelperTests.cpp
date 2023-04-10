ca08a6caaf15aaf8bdd3ab3eddcbd1c8300ef331
andrew-guz <andrew.guz.spb@gmail.com>
2023-04-10 13:56:20 +0300

    Squashed commit of the following:
    commit 11781e8b8f8c09f6b1eaa7a3f40836445e270b7e
    Author: andrew-guz <andrew.guz.spb@gmail.com>
    Date:   Mon Apr 10 13:49:37 2023 +0300
    
        cleanup test.
    
    commit 93b705e4eab027e39ca06618fc88863531403683
    Author: andrew-guz <andrew.guz.spb@gmail.com>
    Date:   Mon Apr 10 13:48:05 2023 +0300
    
        TimeHelper tests updated.
    
    commit 717898101403121dba1bee69f0b06f3eb255e597
    Author: andrew-guz <andrew.guz.spb@gmail.com>
    Date:   Mon Apr 10 13:16:58 2023 +0300
    
        Introduce simple Unit test system for base library.

---
+++ Cpp/UniversalDeviceLib/tests/TimeHelperTests.cpp
@@ -0,0 +1,50 @@
+#define CONFIG_CATCH_MAIN
+#include "catch.hpp"
+
+#include "TimeHelper.h"
+
+TEST_CASE()
+{
+    auto now = std::chrono::system_clock::now();
+
+    auto intValue1 = TimeHelper::TimeToInt(now);
+    auto timeValue = TimeHelper::TimeFromInt(intValue1);
+    auto delta = now - timeValue;
+    delta = std::chrono::duration_cast<std::chrono::seconds>(delta);
+    REQUIRE(delta.count() == 0);
+
+    auto intValue2 = TimeHelper::TimeToInt(timeValue);
+    REQUIRE(intValue1 == intValue2);
+}
+
+TEST_CASE()
+{
+    auto now = std::chrono::system_clock::now();
+
+    auto stringValue1 = TimeHelper::TimeToString(now);    
+    auto timeValue = TimeHelper::TimeFromString(stringValue1);
+    auto delta = now - timeValue;
+    delta = std::chrono::duration_cast<std::chrono::seconds>(delta);
+    REQUIRE(delta.count() == 0);
+
+    auto stringValue2 = TimeHelper::TimeToString(timeValue);
+    REQUIRE(stringValue1 == stringValue2);
+}
+
+TEST_CASE()
+{
+    auto now = std::chrono::system_clock::now();
+
+    auto intValue = TimeHelper::TimeToInt(now);
+    auto stringValue1 = TimeHelper::TimeToString(now);
+    auto stringValue2 = TimeHelper::TimeToString(intValue);
+    REQUIRE(stringValue1 == stringValue2);
+}
+
+TEST_CASE()
+{
+    auto time = TimeHelper::TimeFromString("01-01-2023 01:23:56");
+    auto[hour, minute] = TimeHelper::GetHourMinute(time);
+    REQUIRE(hour == 1);
+    REQUIRE(minute == 23);
+}
