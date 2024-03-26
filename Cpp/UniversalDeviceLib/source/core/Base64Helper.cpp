7fc6d1f7f7f4903f49ed2f5248d799cb14451bac
andrew-guz <andrew.guz.spb@gmail.com>
2024-03-26 22:04:55 +0300

    Base64 fixed and switched to string_view

--- Cpp/UniversalDeviceLib/source/core/Base64Helper.cpp
+++ Cpp/UniversalDeviceLib/source/core/Base64Helper.cpp
@@ -1,22 +1,25 @@
 #include "Base64Helper.h"
 
 #include <sstream>
+#include <array>
 #include <b64/encode.h>
 #include <b64/decode.h>
 
-std::string Base64Helper::ToBase64(const std::string& str)
+std::string Base64Helper::ToBase64(const std::string_view str)
 {
-    std::istringstream incoming(str);
+    std::istringstream incoming(str.data());
     std::stringstream outgoing;
     base64::encoder encoder;
     encoder.encode(incoming, outgoing);
     outgoing.flush();
-    return outgoing.str();
+    std::string result = outgoing.str();
+    result.erase(result.size() - 1);
+    return result;
 }
 
-std::string Base64Helper::FromBase64(const std::string& str)
+std::string Base64Helper::FromBase64(const std::string_view str)
 {
-    std::istringstream incoming(str);
+    std::istringstream incoming(str.data());
     std::stringstream outgoing;
     base64::decoder decoder;
     decoder.decode(incoming, outgoing);
