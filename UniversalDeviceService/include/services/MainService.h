dd4e407658c53cd89d42f61dfa5c3f969483aa8e
andrew-guz <andrew.guz.spb@gmail.com>
2024-04-21 12:54:41 +0300

    More simple code

--- UniversalDeviceService/include/services/MainService.h
+++ UniversalDeviceService/include/services/MainService.h
@@ -13,10 +13,6 @@ public:
 protected:
     virtual void Initialize(CrowApp& app) override;
 
-    crow::response Version(const crow::request& request);
-
-    crow::response Quit(const crow::request& request, CrowApp& app);
-
 private:
     friend class BaseServiceExtension;
 };
--- UniversalDeviceService/source/services/MainService.cpp
+++ UniversalDeviceService/source/services/MainService.cpp
@@ -5,13 +5,9 @@
 MainService::MainService(IQueryExecutor* queryExecutor) : BaseService(queryExecutor) {}
 
 void MainService::Initialize(CrowApp& app) {
-    CROW_ROUTE(app, API_VERSION).methods(crow::HTTPMethod::GET)([&](const crow::request& request) { return Version(request); });
-    CROW_ROUTE(app, API_QUIT).methods(crow::HTTPMethod::GET)([&](const crow::request& request) { return Quit(request, app); });
-}
-
-crow::response MainService::Version(const crow::request& request) { return crow::response(crow::OK, VERSION); }
-
-crow::response MainService::Quit(const crow::request& request, CrowApp& app) {
-    app.stop();
-    return crow::response(crow::OK);
+    CROW_ROUTE(app, API_VERSION).methods(crow::HTTPMethod::GET)([]() { return crow::response(crow::OK, VERSION); });
+    CROW_ROUTE(app, API_QUIT).methods(crow::HTTPMethod::GET)([&]() {
+        app.stop();
+        return crow::response(crow::OK);
+    });
 }
