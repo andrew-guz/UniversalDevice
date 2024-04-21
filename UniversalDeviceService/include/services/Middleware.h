c4f38ee6ac84d6b988bd3ae777759723fedf0afe
andrew-guz <andrew.guz.spb@gmail.com>
2024-04-21 13:01:38 +0300

    Allow to ask version without authorization.

--- UniversalDeviceService/include/services/Middleware.h
+++ UniversalDeviceService/include/services/Middleware.h
@@ -3,12 +3,16 @@
 
 #include <crow.h>
 
+#include "Defines.h"
 #include "AccountManager.h"
 
 struct Authorization {
     struct context {};
 
     void before_handle(crow::request& request, crow::response& response, context& context) {
+        if (request.url == API_VERSION)
+            return;
+
         auto authorization = request.get_header_value("Authorization");
         if (authorization.empty() || !AccountManager::Instance()->IsValidUser(authorization)) {
             response.code = crow::status::UNAUTHORIZED;
