45eafdb3f76dfd203f3383cea92cff36378c79b6
andrew-guz <andrew.guz.spb@gmail.com>
2024-08-16 12:58:16 +0300

    Write in log information that service started of finished.

--- UniversalDeviceFrontendService/main.cpp
+++ UniversalDeviceFrontendService/main.cpp
@@ -4,7 +4,13 @@
 int main(int argc, char** argv) {
     Logger::SetLogLevel(LogLevel::INFO);
 
+    LOG_INFO << "Starting Client service..." << std::endl;
+
     auto settings = Settings::ReadSettings();
 
-    return Wt::WRun(argc, argv, [&](const Wt::WEnvironment& env) { return std::make_unique<Application>(settings, env); });
+    const auto result = Wt::WRun(argc, argv, [&](const Wt::WEnvironment& env) { return std::make_unique<Application>(settings, env); });
+
+    LOG_INFO << "Stopping Client service" << std::endl;
+
+    return result;
 }
