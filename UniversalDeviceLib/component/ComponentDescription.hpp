f2bca4d7beab052e721eaa59fd77e3995c954cd0
andrew-guz <andrew.guz.spb@gmail.com>
2024-08-23 11:20:43 +0300

    Comment changed

--- UniversalDeviceLib/component/ComponentDescription.hpp
+++ UniversalDeviceLib/component/ComponentDescription.hpp
@@ -5,7 +5,8 @@
 #include "Uuid.hpp"
 
 struct ComponentDescription {
-    // TODOAGZ : this 99% is Device Typ like Relay but can be TimerEvent. That why this was a string....
+    // This 99% is Device Type, like Relay or Thermometer, but can be TimerEvent. That why this was a string.
+    // Also devices use this structure to send data
     std::string _type;
     Uuid _id;
 
