e829c7395d0d0155f20ec885a90b190469e5dfd4
andrew-guz <andrew.guz.spb@gmail.com>
2024-08-27 14:29:46 +0300

    Update

--- UniversalDeviceService/processors/ThermometerProcessor.cpp
+++ UniversalDeviceService/processors/ThermometerProcessor.cpp
@@ -1,10 +1,7 @@
 #include "ThermometerProcessor.hpp"
 
-#include <sstream>
+#include <fmt/format.h>
 
-#include <nlohmann/json_fwd.hpp>
-
-#include "Constants.hpp"
 #include "DeviceInformationDescription.hpp"
 #include "ExtendedThermometerCurrentValue.hpp"
 #include "Logger.hpp"
@@ -29,7 +26,7 @@ nlohmann::json ThermometerProcessor::ProcessMessage(const std::chrono::system_cl
         case Subject::WebSocketGetCommands:
             return {};
     }
-    LOG_ERROR << "Unknown subject to process in ThermometerProcessor: " << static_cast<int>(message._header._subject) << std::endl;
+    LOG_ERROR_MSG(fmt::format("Unknown subject to process in ThermometerProcessor: {}", static_cast<int>(message._header._subject)));
     return {};
 }
 
@@ -37,20 +34,18 @@ nlohmann::json ThermometerProcessor::ProcessThermometerCurrentValueMessage(const
                                                                            const Message& message) {
     auto currentValue = message._data.get<ThermometerCurrentValue>();
     if (currentValue._value == std::numeric_limits<float>::min()) {
-        LOG_ERROR << "ThermometerProcessor - invalid message." << std::endl;
+        LOG_ERROR_MSG("ThermometerProcessor - invalid message.");
         return {};
     }
     auto& description = message._header._description;
     if (std::abs(currentValue._value - -127.0f) < 0.1f) {
-        LOG_INFO << "-127.0 found - no sensor connected to " << description._id.data() << "." << std::endl;
+        LOG_INFO_MSG(fmt::format("-127.0 found - no sensor connected to {}.", description._id.data()));
         return {};
     }
-    std::stringstream queryStream;
-    queryStream << "INSERT INTO Thermometers (id, timestamp, value) VALUES ('" << description._id.data() << "', " << TimeHelper::TimeToInt(timestamp)
-                << ", '" << currentValue._value << "')";
-    queryStream.flush();
-    if (!_queryExecutor->Execute(queryStream.str())) {
-        LOG_SQL_ERROR(queryStream.str());
+    const std::string query = fmt::format("INSERT INTO Thermometers (id, timestamp, value) VALUES ('{}', {}, '{}'", description._id.data(),
+                                          TimeHelper::TimeToInt(timestamp), currentValue._value);
+    if (!_queryExecutor->Execute(query)) {
+        LOG_SQL_ERROR(query);
         return {};
     }
     return nlohmann::json{
@@ -67,29 +62,26 @@ nlohmann::json ThermometerProcessor::ProcessGetDeviceInformationMessage(const st
     if (description._seconds != 0) {
         auto now = std::chrono::system_clock::now();
         now -= std::chrono::seconds(description._seconds);
-        std::stringstream queryStream;
-        queryStream << "SELECT timestamp, value FROM Thermometers WHERE id = '" << description._id.data()
-                    << "' AND timestamp >= " << TimeHelper::TimeToInt(now) << " AND value > -126.9 ORDER BY idx DESC";
-        queryStream.flush();
+        const std::string query =
+            fmt::format("SELECT timestamp, value FROM Thermometers WHERE id = '{}' AND timestamp >= {} AND value > -126.9 ORDER BY idx DESC",
+                        description._id.data(), TimeHelper::TimeToInt(now));
         std::vector<std::vector<std::string>> data;
-        if (_queryExecutor->Select(queryStream.str(), data))
+        if (_queryExecutor->Select(query, data))
             extendedThermometerCurrentValues = DbExtension::CreateVectorFromDbStrings<ExtendedThermometerCurrentValue>(data);
         else
-            LOG_SQL_ERROR(queryStream.str());
+            LOG_SQL_ERROR(query);
     }
     if (extendedThermometerCurrentValues.size() == 0) {
-        std::stringstream queryStream;
-        queryStream << "SELECT timestamp, value FROM Thermometers WHERE id = '" << description._id.data()
-                    << "' AND value > -126.9 ORDER BY idx DESC LIMIT 1";
-        queryStream.flush();
+        const std::string query = fmt::format(
+            "SELECT timestamp, value FROM Thermometers WHERE id = '{}' AND value > -126.9 ORDER BY idx DESC LIMIT 1", description._id.data());
         std::vector<std::vector<std::string>> data;
-        if (_queryExecutor->Select(queryStream.str(), data))
+        if (_queryExecutor->Select(query, data))
             extendedThermometerCurrentValues = DbExtension::CreateVectorFromDbStrings<ExtendedThermometerCurrentValue>(data);
         else
-            LOG_SQL_ERROR(queryStream.str());
+            LOG_SQL_ERROR(query);
     }
     if (extendedThermometerCurrentValues.size())
         return extendedThermometerCurrentValues;
-    LOG_INFO << "No data for device " << description._id.data() << "found." << std::endl;
+    LOG_INFO_MSG(fmt::format("No data for device {} found", description._id.data()));
     return {};
 }
