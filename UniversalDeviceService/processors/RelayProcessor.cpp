ccc24b33220d50b9745f4145fe186ce6d2a18472
andrew-guz <andrew.guz.spb@gmail.com>
2024-08-27 13:52:14 +0300

    Update

--- UniversalDeviceService/processors/RelayProcessor.cpp
+++ UniversalDeviceService/processors/RelayProcessor.cpp
@@ -1,8 +1,7 @@
 #include "RelayProcessor.hpp"
 
-#include <sstream>
+#include <fmt/format.h>
 
-#include "Constants.hpp"
 #include "DeviceInformationDescription.hpp"
 #include "ExtendedRelayCurrentState.hpp"
 #include "Logger.hpp"
@@ -27,23 +26,21 @@ nlohmann::json RelayProcessor::ProcessMessage(const std::chrono::system_clock::t
         case Subject::WebSocketGetCommands:
             return {};
     }
-    LOG_ERROR << "Unknown subject to process in RelayProcessor: " << static_cast<int>(message._header._subject) << std::endl;
+    LOG_ERROR_MSG(fmt::format("Unknown subject to process in RelayProcessor: {}", static_cast<int>(message._header._subject)));
     return {};
 }
 
 nlohmann::json RelayProcessor::ProcessRelayCurrentStateMessage(const std::chrono::system_clock::time_point& timestamp, const Message& message) {
     auto currentState = message._data.get<RelayCurrentState>();
     if (currentState._state == std::numeric_limits<float>::min()) {
-        LOG_ERROR << "RelayProcessor - invalid message." << std::endl;
+        LOG_ERROR_MSG("RelayProcessor - invalid message.");
         return {};
     }
     auto& description = message._header._description;
-    std::stringstream queryStream;
-    queryStream << "INSERT INTO Relays (id, timestamp, state) VALUES ('" << description._id.data() << "', " << TimeHelper::TimeToInt(timestamp)
-                << ", '" << currentState._state << "')";
-    queryStream.flush();
-    if (!_queryExecutor->Execute(queryStream.str())) {
-        LOG_SQL_ERROR(queryStream.str());
+    const std::string query = fmt::format("INSERT INTO Relays (id, timestamp, state) VALUES ('{}', {}, '{}'", description._id.data(),
+                                          TimeHelper::TimeToInt(timestamp), currentState._state);
+    if (!_queryExecutor->Execute(query)) {
+        LOG_SQL_ERROR(query);
         return {};
     }
     return nlohmann::json{
@@ -59,28 +56,25 @@ nlohmann::json RelayProcessor::ProcessGetDeviceInformationMessage(const std::chr
     if (description._seconds != 0) {
         auto now = std::chrono::system_clock::now();
         now -= std::chrono::seconds(description._seconds);
-        std::stringstream queryStream;
-        queryStream << "SELECT timestamp, state FROM Relays WHERE id = '" << description._id.data()
-                    << "' AND timestamp >= " << TimeHelper::TimeToInt(now) << " ORDER BY idx DESC";
-        queryStream.flush();
+        const std::string query = fmt::format("SELECT timestamp, state FROM Relays WHERE id = '{}' AND timestamp >= {} ORDER BY idx DESC",
+                                              description._id.data(), TimeHelper::TimeToInt(now));
         std::vector<std::vector<std::string>> data;
-        if (_queryExecutor->Select(queryStream.str(), data))
+        if (_queryExecutor->Select(query, data))
             extendedRelayCurrentStates = DbExtension::CreateVectorFromDbStrings<ExtendedRelayCurrentState>(data);
         else
-            LOG_SQL_ERROR(queryStream.str());
+            LOG_SQL_ERROR(query);
     }
     if (extendedRelayCurrentStates.size() == 0) {
-        std::stringstream queryStream;
-        queryStream << "SELECT timestamp, state FROM Relays WHERE id = '" << description._id.data() << "' ORDER BY idx DESC LIMIT 1";
-        queryStream.flush();
+        const std::string query =
+            fmt::format("SELECT timestamp, state FROM Relays WHERE id = '{}' ORDER BY idx DESC LIMIT 1", description._id.data());
         std::vector<std::vector<std::string>> data;
-        if (_queryExecutor->Select(queryStream.str(), data))
+        if (_queryExecutor->Select(query, data))
             extendedRelayCurrentStates = DbExtension::CreateVectorFromDbStrings<ExtendedRelayCurrentState>(data);
         else
-            LOG_SQL_ERROR(queryStream.str());
+            LOG_SQL_ERROR(query);
     }
     if (extendedRelayCurrentStates.size())
         return extendedRelayCurrentStates;
-    LOG_INFO << "No data for device " << description._id.data() << "found." << std::endl;
+    LOG_INFO_MSG(fmt::format("No data for device {} found", description._id.data()));
     return {};
 }
