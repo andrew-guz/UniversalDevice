9c7f8f89ca21aef0a5dbb559de887357b9b91a54
andrew-guz <andrew.guz.spb@gmail.com>
2024-09-26 09:58:49 +0300

    Configure script updated

--- .vscode/configure.sh
+++ .vscode/configure.sh
@@ -1,3 +1,3 @@
-mkdir -p build && cd build && cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++-17 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --fresh ..
+mkdir -p build && cd build && cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++-17 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --fresh ..
 
 ../.vscode/notify.sh $?
