ceae771515f059c47f3e47f327c74a80ba1eb92b
andrew-guz <andrew.guz.spb@gmail.com>
2024-10-01 12:50:11 +0300

    Format script updated

--- .vscode/format.sh
+++ .vscode/format.sh
@@ -1,6 +1,9 @@
 cmake_files=`find . -name CMakeLists.txt -not -path "*/vendor/*"`
 
-/usr/bin/clang-format-17 -i --verbose src/**/*.cpp src/**/*.hpp && /usr/bin/cmake-format -l error -i $cmake_files
+FILES=$(find ./src -name "*.*pp")
+echo $FILES
+
+/usr/bin/clang-format-17 -i --verbose $FILES && /usr/bin/cmake-format -l error -i $cmake_files
 
 ./.vscode/notify.sh $?
 
