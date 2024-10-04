b70ddcd88451f56815cf5671c3f4eb8327abb504
andrew-guz <andrew.guz.spb@gmail.com>
2024-10-04 14:41:48 +0300

    Scenarios table cache tests

--- src/UniversalDeviceTests/ScenariosTableStorageCacheTest.cpp
+++ src/UniversalDeviceTests/ScenariosTableStorageCacheTest.cpp
@@ -6,6 +6,8 @@
 #include "Scenario.hpp"
 #include "SimpleTableStorageCache.hpp"
 #include "Storage.hpp"
+#include "StorageCacheSharedData.hpp"
+#include "Uuid.hpp"
 
 TEST_CASE("ScenariosTableStorageCacheTest") {
     std::filesystem::path dbPath = std::filesystem::temp_directory_path() / "test.db";
@@ -16,10 +18,65 @@ TEST_CASE("ScenariosTableStorageCacheTest") {
 
     IStorageCache* storageCache = GetScenariosCache(&storage);
 
-    SimpleTableSelectAllOutput<Scenario> selectAllResult;
-    const StorageCacheProblem selectAll = storageCache->SelectAll(selectAllResult);
-    REQUIRE(selectAll._type == StorageCacheProblemType::NoProblems);
-    REQUIRE(selectAllResult._data.size() == 0);
+    Scenario scenario{
+        ._name = "test",
+    };
+
+    auto checkSelect = [&scenario, &storageCache](bool contains) {
+        SimpleTableSelectAllOutput<Scenario> selectAllResult;
+        const StorageCacheProblem selectAllProblem = storageCache->SelectAll(selectAllResult);
+        REQUIRE(selectAllProblem._type == StorageCacheProblemType::NoProblems);
+        if (contains) {
+            REQUIRE(selectAllResult._data.size() == 1);
+            REQUIRE(selectAllResult._data.at(0) == scenario);
+
+            const SimpleTableSelectInput selectInput{
+                ._id = scenario._id,
+            };
+            SimpleTableSelectOutput<Scenario> selectOutput;
+            const StorageCacheProblem selectProblem = storageCache->Select(selectInput, selectOutput);
+            REQUIRE(selectProblem._type == StorageCacheProblemType::NoProblems);
+            REQUIRE(selectOutput._data == scenario);
+        } else {
+            REQUIRE(selectAllResult._data.size() == 0);
+        }
+    };
+
+    checkSelect(false);
+
+    {
+        const SimpleTableInsertOrReplaceInput<Scenario> insert{
+            ._id = scenario._id,
+            ._data = scenario,
+        };
+        const StorageCacheProblem insertProblem1 = storageCache->InsertOrReplace(insert);
+        REQUIRE(insertProblem1._type == StorageCacheProblemType::NoProblems);
+    }
+
+    checkSelect(true);
+
+    scenario._activateEvent.emplace(Uuid{});
+
+    {
+        const SimpleTableUpdateInput<Scenario> input{
+            ._id = scenario._id,
+            ._data = scenario,
+        };
+        const StorageCacheProblem updateProblem = storageCache->Update(input);
+        REQUIRE(updateProblem._type == StorageCacheProblemType::NoProblems);
+    }
+
+    checkSelect(true);
+
+    {
+        const SimpleTableDeleteInput input{
+            ._id = scenario._id,
+        };
+        const StorageCacheProblem removeProblem = storageCache->Delete(input);
+        REQUIRE(removeProblem._type == StorageCacheProblemType::NoProblems);
+    }
+
+    checkSelect(false);
 
     std::filesystem::remove(dbPath);
 }
