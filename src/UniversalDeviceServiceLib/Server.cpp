0679bedfa399c8939d3215647e06e0e4f3d41c2f
Andrew Guz <andrew.guz.spb@gmail.com>
2024-10-17 21:45:36 +0300

    Fix after #103

---
+++ src/UniversalDeviceServiceLib/Server.cpp
@@ -0,0 +1,49 @@
+#include "Server.hpp"
+
+#include <crow.h>
+#include <fmt/format.h>
+
+#include "AccountManagerInitializer.hpp"
+#include "ClientService.hpp"
+#include "DeviceService.hpp"
+#include "Logger.hpp"
+#include "MainService.hpp"
+#include "Middleware.hpp"
+#include "PathHelper.hpp"
+#include "Settings.hpp"
+#include "Storage.hpp"
+
+int Server::run() {
+    try {
+        auto settings = Settings::ReadSettings();
+        if (!settings._logPath.empty())
+            PathHelper::SetCustomLogPath(settings._logPath);
+
+        Logger::SetLogLevel(LogLevel::INFO);
+
+        LOG_INFO_MSG("Starting Device service...");
+
+        AccountManager::Instance()->Init(std::make_shared<AccountManagerInitializer>(PathHelper::FullFilePath(settings._authPath)));
+
+        Storage storage(PathHelper::FullFilePath(settings._dbPath));
+
+        CrowApp app;
+
+        BaseServiceExtension::Create<MainService>(app, &storage);
+        BaseServiceExtension::Create<DeviceService>(app, &storage);
+        BaseServiceExtension::Create<ClientService>(app, &storage);
+
+        app.ssl_file(PathHelper::FullFilePath(settings._certificatePath).native(), PathHelper::FullFilePath(settings._keyPath).native())
+            .port(settings._port)
+            .multithreaded()
+            .run();
+
+        LOG_INFO_MSG("Stopping Device service");
+    } catch (const std::exception& ex) {
+        LOG_ERROR_MSG(fmt::format("Exception caught: '{}'", ex.what()));
+    } catch (...) {
+        LOG_ERROR_MSG("Unknown exception caught");
+    }
+
+    return 0;
+}
